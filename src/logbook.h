4e6264efca49a29049e7bc7092b8c0ffd5ee81cf
Shawn Rutledge LB2JK / K7IHZ <s@ecloud.org>
2025-03-27 10:17:31 +0100

    Add last QSO date and age to long-press popover

--- src/logbook.h
+++ src/logbook.h
@@ -2,7 +2,8 @@ void logbook_add(char *contact_callsign, char *rst_sent, char *exchange_sent,
 	char *rst_recv, char *exchange_recv, int tx_power, int tx_vswr, char *comments);
 int logbook_query(char *query, int from_id, char *result_file);
 int logbook_count_dup(const char *callsign, int last_seconds);
-int logbook_prev_log(const char *callsign, char *result);
+int logbook_prev_log(char *out, int outlen, const char *callsign);
+int logbook_last_date(struct tm *out, const char *callsign);
 int logbook_get_grids(void (*f)(char *,int));
 void logbook_list_open();
 void logbook_open();
