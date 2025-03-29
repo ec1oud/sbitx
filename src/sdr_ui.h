461ddca39c360789fde797f142404fb394ed34c4
Shawn Rutledge LB2JK / K7IHZ <s@ecloud.org>
2025-03-29 12:45:09 +0100

    Add modes/ft8/1/received.meta/spans
    Each span is a 64-bit word in 32|16|8|8 little-endian format:
    line, start_column, length, semantic
    (can be cast to the text_span_semantic struct on little-endian platforms)
    
    I think it gets out of sync when the console goes past MAX_CONSOLE_LINES
    and wraps, though. The debug output indicates that when we
    `cat modes/ft8/1/received` we end on a different line than the line
    value from the end of `hexdump -C modes/ft8/1/received.meta/spans`
    (perhaps only off-by-one each time).

--- src/sdr_ui.h
+++ src/sdr_ui.h
@@ -91,7 +91,9 @@ void write_console(sbitx_style style, const char *text);
 // write plain text, with semantically-tagged spans that imply styling
 void write_console_semantic(const char *text, const text_span_semantic *sem, int sem_count);
 int get_console_text(char *buf, int max, int from_char, sbitx_style filter);
+int get_console_text_spans(text_span_semantic *buf, int max, int from_char, sbitx_style filter);
 int console_current_length(sbitx_style filter);
+int console_current_spans_length(sbitx_style filter);
 time_t console_last_time();
 int web_get_console(char *buff, int max);
 int is_in_tx();
