cda7b7dff4f8bb4885b1443aa564d3c241ec578c
Shawn Rutledge LB2JK / K7IHZ <s@ecloud.org>
2025-04-25 23:08:56 +0200

    Fix crash in change_band()
    If old_freq is not in band_stack, e.g. after tuning out-of-band (SWL),
    old_band == max_bands at the end of the search loop; so we cannot store
    the current frequency in the band stack before switching.
    
    Amends 27006ea31759ffc0435b587ee4e7bc5c3306eac1

--- src/sbitx_gtk.c
+++ src/sbitx_gtk.c
@@ -7497,11 +7497,11 @@ void change_band(char *request)
 		if (band_stack[old_band].start <= old_freq && old_freq <= band_stack[old_band].stop)
 			break;
 
-	int stack = band_stack[old_band].index;
-	if (stack < 0 || stack >= STACK_DEPTH)
-		stack = 0;
-	if (old_band < max_bands)
-	{
+	int stack = 0;
+	if (old_band < max_bands) {
+		stack = band_stack[old_band].index;
+		if (stack < 0 || stack >= STACK_DEPTH)
+			stack = 0;
 		// update the old band setting
 		if (stack >= 0 && stack < STACK_DEPTH)
 		{
